de35907f3a11a4db10a7e35330b3e10c70521337
jfitzsimmons <joefitzsimmons@Corinnes-MacBook-Pro.local>
2018-08-12 16:53:16 -0500

    update beginning draw func

--- main.js
+++ main.js
@@ -63,20 +63,22 @@ function collision(head,array){
 
 //draw to canvas
 function draw(){
-  console.log('1st snake 0 x ' + snake[0].x);
-  ctx.drawImage(groundImg,0,0);
-  for(let i=0; i<snake.length; i++){
-    ctx.fillStyle = (i==0) ? 'green' : 'white';
-    ctx.fillRect(snake[i].x,snake[i].y,box,box);
+
+  ctx.drawImage(ground,0,0);
+
+  for( let i = 0; i < snake.length ; i++){
+      ctx.fillStyle = ( i == 0 )? "green" : "white";
+      ctx.fillRect(snake[i].x,snake[i].y,box,box);
+
+      ctx.strokeStyle = "red";
+      ctx.strokeRect(snake[i].x,snake[i].y,box,box);
   }
-  ctx.drawImage(foodImg,food.x,food.y);
 
-  console.log('2nd snake 0 x ' + snake[0].x);
+  ctx.drawImage(foodImg, food.x, food.y);
 
-  //old head position
+  // old head position
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
-
   //find new snake head direction???
   if (d="lEFT") snakeX-=box;
   if (d="UP") snakeY-=box;

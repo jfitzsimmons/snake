3341f1748d0f2eff24828a93a09588db0dd07ae4
jfitzsimmons <joefitzsimmons@Corinnes-MacBook-Pro.local>
2018-08-12 16:24:11 -0500

    final all files

--- main.js
+++ main.js
@@ -1,48 +1,34 @@
-const cvs =document.getElemetnById('canvas');
-
+const cvs =document.getElemetnById('snake');
 const ctx = cvs.getContext('2d');
+const box = 32;
+
 //Load images
 //add images | should audio be audio file?
-let imageName = new Image();
-imageName.src = 'path/img.png';
-let audioName = new Audio();
-audioName.src = 'path/audio.png'; audioName.play();
+const groundImg = new Image();
+groundImg.src = "img/ground.png";
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
-//draw images
-ctx.drawImage(imageName, 40,50,25,25);
+//load audio
+const deadAud = new Audio();
+const eatAud = new Audio();
+const controlAud = new Audio();
 
-//Draw rectangle
-ctx.fillStyle = 'red';
-ctx.fillRect (100,300,30,30);
+deadAud.src = 'audio/dead.mp3';
+eatAud.src = 'audio/eat.mp3';
+controlAud.src = 'audio/control.mp3';
 
-//unit box
-let box = 32
-
-ctx.fillStyle = 'black';
-ctx.fillRect(5*box,6*box,2*box,3*box);
-
-//snake ground food score
+//creat snake
 let snake = [];
 snake[0] = {x: 9*box, y:10*box};
-snake[1] = {x: 8*box, y:10*box};
+
+//create food
 let food = {
   x: Math.floor(Math.random()*17+1)*box
   y: Math.floor(Math.random()*15+3)*box };
+
+//create score
 let score = 0;
-
-function draw(){
-  ctx.drawImage(ground,0,0);
-  for(let i=0; i<snake.length;i++){
-    ctx.fillStyle = (i==0)?'green' : 'white';
-    ctx.fillRect(snake[i].x,snake[i].y,box,box);
-  }
-  ctx.drawImage(foodImg,food.x,food.y);
-  ctx.fillStyle='white';
-  ctx.font='45px Changa One'
-  ctx.fillText(score,2*box,1.6*box);
-}
-
-let game = setInterval(draw,100);
 
 //controls
 let d;
@@ -50,38 +36,77 @@ document.addEventListener('keydown',direction);
 
 function direction(event){
   if(event.keyCode==37 && d!='RIGHT'){
+    controlAud.play();
     d = 'LEFT';}
   else if(event.keyCode==38 && d!='DOWN'){
+    controlAud.play();
     d = 'UP';}
   else if(event.keyCode==39 && d!='LEFT'){
+    controlAud.play();
     d = 'RIGHT';}
   else if(event.keyCode==40 && d!='UP'){
+    controlAud.play();
     d = 'DOWN';}
 }
 
-//snake movement
-snake.pop();
-snakeX = snake[0].x;
-snakeY = snake[0].y;
 
-//find new snake head direction???
-//sankeX+=box;
-//sankeX-=box;
-//sankeY+=box;
-//sankeY-=box;
-
-let newHead = {
-  x:snakeX,
-  y:snakeY
-}
-snake.unshift(newHead);
-
-//eats food
-if (snakeX == food.x && snakeY == fod.y){
-  score++;
-  food={
-    x:unit*Math.floor(Math.random()*17+1),
-    y:unit*Math.floor(Math.random()*15+3)
+function collision(head,array){
+  for (let i=0; i<array.length;i++){
+    if(head.x == array[i].x && head.y == array[i].y){
+      return true;
+    }
   }
-  //add newHead without snake.pop()
+  return false;
 }
+//check for border collisions too
+
+
+//draw to canvas
+function draw(){
+  ctx.drawImage(ground,0,0);
+  for(let i=0; i<snake.length;i++){
+    ctx.fillStyle = (i==0)?'green' : 'white';
+    ctx.fillRect(snake[i].x,snake[i].y,box,box);
+  }
+  ctx.drawImage(foodImg,food.x,food.y);
+
+  //old head position
+  //snake movement
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  //find new snake head direction???
+  if (d="lEFT") snakeX-=box;
+  if (d="UP") snakeY-=box;
+  if (d="RIGHT") snakeX+=box;
+  if (d="DOWN") snakeY+=box;
+
+  //eats food
+  if (snakeX == food.x && snakeY == food.y){
+    score++;
+    eatAud.play();
+    food={
+      x:unit*Math.floor(Math.random()*17+1),
+      y:unit*Math.floor(Math.random()*15+3)
+    }else{snake.pop();}
+
+    let newHead = {
+      x:snakeX,
+      y:snakeY
+    }
+
+    //game over
+    if(snakeX<box || snakeX>17*box || snakeY<3*box || snakeY>17*box || collison(newHead,snake)){
+      clearInterval(game);
+      deadAud.play();
+    }
+
+  snake.unshift(newHead);
+
+  ctx.fillStyle='white';
+  ctx.font='45px Changa One'
+  ctx.fillText(score,2*box,1.6*box);
+}
+
+let game = setInterval(draw,100);

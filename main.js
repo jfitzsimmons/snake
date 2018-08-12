b615d8a66c62102fdc3667f6319681db5e99bdd3
jfitzsimmons <joefitzsimmons@Corinnes-MacBook-Pro.local>
2018-08-12 16:33:59 -0500

    bad var name ln66

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function collision(head,array){
 
 //draw to canvas
 function draw(){
-  ctx.drawImage(ground,0,0);
+  ctx.drawImage(groundImg,0,0);
   for(let i=0; i<snake.length;i++){
     ctx.fillStyle = (i==0)?'green' : 'white';
     ctx.fillRect(snake[i].x,snake[i].y,box,box);
